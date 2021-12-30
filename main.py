e0bf0685a1cb07f4d466069e7417106869f0a50e
Raniita <efdezsanchez@gmail.com>
2021-12-30 19:22:33 +0100

    fix: about

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def send_about(message: types.Message):
     logging.info('Sending about to: @{}<{}>'.format(message.chat.username, message.chat.id))
     content.append(text('Gotify Client for Telegram. Connected to: ', code(GOTIFY_URL), ' :check_mark:'))
 
-    await telegram_bot.send_message(message.chat.id, emojize(content), parse_mode=ParseMode.MARKDOWN)
+    await telegram_bot.send_message(message.chat.id, emojize(text(*content, sep='\n')), parse_mode=ParseMode.MARKDOWN)
 
 
 @dispatcher.message_handler()
